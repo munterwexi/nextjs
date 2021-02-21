79f198df8fa1ead380f0ac521c20f37810709eed
munterwexi <79418281+munterwexi@users.noreply.github.com>
2021-02-21 22:04:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://unterwexi.com">unterwexi.com!</a>
+          Welcome to <a href="https://unterwexi.com">unterwexi.com! test</a>
         </h1>
 
         <p className={styles.description}>
